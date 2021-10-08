0eb5dc9d960db5a1256f9b41976d8b2ba64a8aaa
Justin <justin.curtis.lu@gmail.com>
2021-10-08 12:38:03 -0700

    Set up first Functional Test using Playwright, uses pytest-playwright

--- functional_tests.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    browser = p.firefox.launch()
-    page = browser.new_page()
-    page.goto('http://localhost:8000')
-    assert 'Django' in page.title()
-    browser.close()
