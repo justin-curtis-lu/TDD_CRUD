c619e33c48e79603a4a35c8d4bc1294d78a10305
Justin <justin.curtis.lu@gmail.com>
2021-10-08 19:01:39 -0700

    Add list model sync with items, change views and lists, unique URLs for each list, adjust url layout

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,7 @@ from django.http import HttpRequest
 import pytest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Home View Test Cases
@@ -22,23 +22,6 @@ def test_home_page_html(client):
 
 
 @pytest.mark.django_db
-def test_can_save_a_post_request(client):
-    response = client.post('/', data={'item_text': 'A new list item'})
-
-    assert Item.objects.count() == 1
-    new_item = Item.objects.first()
-    assert new_item.text == 'A new list item'
-
-
-@pytest.mark.django_db
-def test_redirects_after_post(client):
-    response = client.post('/', data={'item_text': 'A new list item'})
-
-    assert response.status_code == 302
-    assert response['location'] == '/lists/the-only-list-in-the-world/'
-
-
-@pytest.mark.django_db
 def test_only_saves_items_when_necessary(client):
     response = client.get('/')
     assert Item.objects.count() == 0
@@ -46,33 +29,101 @@ def test_only_saves_items_when_necessary(client):
 
 # List View Tests Cases
 @pytest.mark.django_db
-def test_displays_all_list_items(client):
-    Item.objects.create(text='test 1')
-    Item.objects.create(text='test 2')
+def test_displays_only_items_for_that_list(client):
+    correct_list = List.objects.create()
+    Item.objects.create(text='itemey 1', list=correct_list)
+    Item.objects.create(text='itemey 2', list=correct_list)
+    other_list = List.objects.create()
+    Item.objects.create(text='other list item 1', list=other_list)
+    Item.objects.create(text='other list item 2', list=other_list)
 
-    response = client.get('/lists/the-only-list-in-the-world/')
+    response = client.get(f'/lists/{correct_list.id}/')
 
-    assertContains(response, 'test 1')
-    assertContains(response, 'test 2')
+    assertContains(response, 'itemey 1')
+    assertContains(response, 'itemey 2')
+    assertNotContains(response, 'other list item 1')
+    assertNotContains(response, 'other list item 2')
 
 
 @pytest.mark.django_db
 def test_uses_list_template(client):
-    response = client.get('/lists/the-only-list-in-the-world/')
+    list_ = List.objects.create()
+    response = client.get(f'/lists/{list_.id}/')
     assertTemplateUsed(response, 'list.html')
 
+@pytest.mark.django_db
+def test_passes_correct_list_to_template(client):
+    other_list = List.objects.create()
+    correct_list = List.objects.create()
+    response = client.get(f'/lists/{correct_list.id}/')
+    assert response.context['list'] == correct_list
 
 
-# Model Test Cases
+# New Item Test Case
+@pytest.mark.django_db
+def test_can_save_a_POST_request_to_an_existing_list(client):
+    other_list = List.objects.create()
+    correct_list = List.objects.create()
+
+    client.post(
+        f'/lists/{correct_list.id}/add_item',
+        data={'item_text': 'A new item for an existing list'}
+    )
+
+    assert Item.objects.count() == 1
+    new_item = Item.objects.first()
+    assert new_item.text == 'A new item for an existing list'
+    assert new_item.list == correct_list
+
+
+@pytest.mark.django_db
+def test_redirects_to_list_view(client):
+    other_list = List.objects.create()
+    correct_list = List.objects.create()
+
+    response = client.post(
+        f'/lists/{correct_list.id}/add_item',
+        data={'item_text': 'A new item for an existing list'}
+    )
+
+    assertRedirects(response, f'/lists/{correct_list.id}/')
+
+
+# New List Test Cases
+@pytest.mark.django_db
+def test_can_save_a_POST_request(client):
+    client.post('/lists/new', data={'item_text': 'A new list item'})
+    assert Item.objects.count() == 1
+    new_item = Item.objects.first()
+    assert new_item.text == 'A new list item'
+
+
+@pytest.mark.django_db
+def test_redirects_after_POST(client):
+    response = client.post('/lists/new', data={'item_text': 'A new list item'})
+    new_list = List.objects.first()
+    assertRedirects(response, f'/lists/{new_list.id}/')
+
+
+# List & Model Test Cases
 @pytest.mark.django_db
 def test_saving_and_retrieving_items(client):
+    list_ = List()
+    list_.save()
+
     first_item = Item()
     first_item.text = 'The first (ever) list item'
+    first_item.list = list_
     first_item.save()
 
     second_item = Item()
     second_item.text = 'Item the second'
+    second_item.list = list_
     second_item.save()
+
+    saved_list = List.objects.first()
+    assert saved_list == list_
+
 
     saved_items = Item.objects.all()
     assert saved_items.count() == 2
@@ -80,4 +131,6 @@ def test_saving_and_retrieving_items(client):
     first_saved_item = saved_items[0]
     second_saved_item = saved_items[1]
     assert first_saved_item.text == 'The first (ever) list item'
+    assert first_saved_item.list == list_
     assert second_saved_item.text == 'Item the second'
+    assert second_saved_item.list == list_
